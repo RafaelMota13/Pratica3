767ef101043d15402ad06e8c595760f703fa12e1
Rafael <faelmota7@gmail.com>
2022-09-23 21:18:19 -0300

    23/09

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Security.Cryptography;
+
+int valorDoDado = RandomNumberGenerator.GetInt32(1,7);
+
+Console.WriteLine($"O seu dado cai na face {valorDoDado}");
+
+double n = 34435.3453453;
+double x = Math.Sqrt(n);
+double y = Math.Sqrt(5) + 2;
+
+// Raiz Quadrada
+double z = Math.Sqrt(5);
+
+// Potência
+Math.Pow(5, 0.5);
+
+int a = 1_000_000;
+
+decimal salario = 1200;
+
+decimal salarioAumento =  salario * 1.1m;
+
+int mega1 = RandomNumberGenerator.GetInt32(1,61);
+int mega2 = RandomNumberGenerator.GetInt32(1,61);
+int mega3 = RandomNumberGenerator.GetInt32(1,61);
+int mega4 = RandomNumberGenerator.GetInt32(1,61);
+int mega5 = RandomNumberGenerator.GetInt32(1,61);
+int mega6 = RandomNumberGenerator.GetInt32(1,61);
+
+Console.WriteLine($"{mega1} - {mega2} - {mega3} - {mega4} - {mega5} - {mega6}");
+
+
+
+
